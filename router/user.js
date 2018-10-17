7b7d44fa7bb171acc6abe323e6f646b9f98c580b
wcw55320 <a86266122>
2018-10-17 15:03:51 +0800

    路径和方法的绑定

---
+++ router/user.js
@@ -0,0 +1,36 @@
+let userService = require("../service/user");
+let router = require("express").Router();
+let config = require("../config");
+let encryptUtil = require("../utils/encryptUtil");
+
+
+module.exports = router;
+/**
+ * 用户注册
+ * url : POST , http://localhost:8080/
+ * @param user {username:zhangsan,password:123}
+ */
+router.post("/regist", async (req, res) => {
+    let result = await userService.regist(req.body);
+    res.success(result)
+})
+
+
+router.get('/:username', async (req, res) => {
+    let user = await userService.findByUsername(req.params.username);
+    res.success(user)
+});
+
+router.delete('/:username', async (req, res) => {
+    await userService.deleteUserByUsername(req.params.username);
+    res.success()
+});
+
+router.post("/login", async (request, response) => {
+
+    let token = await userService.login(request.body);
+
+    response.success(token);
+
+});
+
