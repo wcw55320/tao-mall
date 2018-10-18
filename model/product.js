1f3bcca065118dd80333d779e4cef480823aff25
wcw55320 <a86266122>
2018-10-18 19:31:46 +0800

    订单模块表结构

---
+++ model/product.js
@@ -0,0 +1,34 @@
+let mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "商品名字不能少"],
+        unique: true
+    },
+    price: {
+        type: String,
+        required: [true, "商品价格不能少"]
+    },
+    stock: { // 库存
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, "分类id不能为空"]
+    },
+    description: {// 商品描述
+        type: String,
+    },
+    isOnSale: {   //是否上架
+        type: Boolean,
+        default: true
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('product', schema)
